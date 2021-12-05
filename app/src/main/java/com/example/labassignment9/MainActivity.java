7ad9ae0f0a66e5ca5e7130cf7e6392e5c11c12c5
greg-navarro <gregorylitmannavarro@gmail.com>
2021-12-04 19:17:25 -0800

    Got onclick listener working

--- app/src/main/java/com/example/labassignment9/MainActivity.java
+++ app/src/main/java/com/example/labassignment9/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -37,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
         cityEditText = (EditText) findViewById(R.id.editTextCity);
         stateEditText = (EditText) findViewById(R.id.editTextState);
         getButton = (Button) findViewById(R.id.button);
+
+        getButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        String city = cityEditText.getText().toString();
+                        String state = stateEditText.getText().toString();
+                        String weatherQuery = city + "," + state;
+                        getData(weatherQuery);
+                        Log.v("MainActivity", "MainActivity - sent weather query!");
+                    }
+                });
+
         Log.d("MainActivity","Main Activity - Entering onCreate");
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Handler handler = new Handler(Looper.getMainLooper());
@@ -50,17 +65,43 @@ public class MainActivity extends AppCompatActivity {
                         new RemoteDataReader( STARTING_URL, city, KEY_NAME, key );
                 //Get the JSON string
                 json= rdr.getData();
-                Log.w("TEST", json);
+                Log.d("TEST", json);
 
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
 
                         TemperatureParser parser = new TemperatureParser( json );
-                        Log.w("MainActivity", String.valueOf(parser.getTemperatureK( ))+ DEGREE+"K");
+                        Log.d("MainActivity", String.valueOf(parser.getTemperatureK( ))+ DEGREE+"K");
                     }
                 });
             }
         });
     } // end oncreate
+
+    public void getData(String newCity) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                //Do background here
+                //String baseUrl = "", cityString = "", keyName = "", key = "";
+                //Create an object RemoteDataReader
+                RemoteDataReader rdr =
+                        new RemoteDataReader( STARTING_URL, newCity, KEY_NAME, key );
+                //Get the JSON string
+                json= rdr.getData();
+                Log.w("TEST", json);
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        TemperatureParser parser = new TemperatureParser( json );
+                        Log.d("MainActivity", String.valueOf(parser.getTemperatureK( ))+ DEGREE+"K success bitch!");
+                    }
+                });
+            }
+        });
+    }
 }
